9d70b98c8a695133d4026ce6c29b69c2cba6ca9f
сахарчук анастасия <_@example.com>
2023-10-25 16:57:52 +0300

    add: weekday search

---
+++ boolean.js
@@ -0,0 +1,8 @@
+const firstNumber = 6
+const secondNumber = 64
+if (firstNumber>secondNumber)
+console.log   ("число " + firstNumber + " больше " + secondNumber)
+else if (firstNumber === secondNumber)
+console.log   ( "данные числа равны ")
+else  
+console.log    (' число ' + firstNumber + ' меньше ' + secondNumber)
---
+++ search_day_week.js
@@ -0,0 +1,17 @@
+const day = 'вася'
+if (day === 'четверг')
+    console.log(`${day} является 4 днем недели`)
+else if (day === 'понедельник')
+    console.log(`${day} является 1 днем недели`)
+else if (day === 'вторник')
+    console.log(`${day} является 2 днем недели`)
+else if (day === 'среда')
+    console.log(`${day} является 3 днем недели`)
+else if (day === 'пятница')
+    console.log(`${day} является 5 днем недели`)
+else if(day === 'суббота')
+    console.log(`${day} является 6 днем недели`)
+else if(day === 'воскресенье')
+    console.log(`${day} является 7 днем недели`)
+else 
+console.log('не правильно введены данные')
