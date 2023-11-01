8cbba85d4ddea93ac1e1d24c49d3bd822b0b2b82
сахарчук анастасия <_@example.com>
2023-11-01 16:57:39 +0300

    add: loop.js

---
+++ loop.js
@@ -0,0 +1,13 @@
+let count = 1
+for(i = 3; i<10 ;i++){
+    console.log(`на ${count} итерации число ${i}`)
+    count++
+}
+
+let shetchik = 1
+for(i=5647; i<5800;i++){
+    if(i%78===0)
+    console.log(`на ${shetchik} итерации ${i}`)
+    shetchik++
+}
+
