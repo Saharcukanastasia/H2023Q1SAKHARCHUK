92f7fd70b5075dca24337d68248bf8fad7e0c6b6
сахарчук анастасия <_@example.com>
2023-11-08 17:21:43 +0300

    add: array

---
+++ array.js
@@ -0,0 +1,7 @@
+const numberArray = [64, 87, 23, 72, 5, 36]
+const result = []
+for (i = 0; i<= numberArray.length -1; i++) {
+    if(numberArray [i] %2===0)
+    result.push (numberArray[i])
+}
+console.log(result)
